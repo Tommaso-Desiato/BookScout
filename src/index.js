c658682ca2097a948ae1162503d172fcbbe35c8a
Tommaso-Desiato <tommaso_desiato@libero.it>
2024-05-16 12:40:07 +0200

    Tentativi per inserire descrizione

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ const apiCall = function (event) {
   //Prevent Default per non far aggiornare la pagina
   event.preventDefault();
   const query = document.getElementById("search-bar").value;
-  const endpoint = `https://openlibrary.org/subjects/${query}.json?limit=10&jscmd=data`;
+  const endpoint = `https://openlibrary.org/subjects/${query}.json?limit=10`;
   axios
     .get(endpoint)
     .then(function (response) {
       _.forEach(response.data.works, (work) => {
         const title = work.title;
         const authorNames = _.map(work.authors, "name");
-        //const description =;
         const bookContainer = document.createElement("div");
         bookContainer.innerHTML = ` ${title} ${authorNames}`;
         document.body.appendChild(bookContainer);
       });
+
       loadMoreBtn.innerHTML = `<button id="load-more">Load More</button>`;
       document.body.appendChild(loadMoreBtn);
       document.getElementById("load-more").addEventListener("click", loadMore);
@@ -27,6 +27,7 @@ const apiCall = function (event) {
       console.log(error);
     });
 };
+
 //Nello scope globale perché richiamato da diverse funzioni
 const loadMoreBtn = document.createElement("button");
 let offset = 0;
