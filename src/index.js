401abb7a5b689b67eb44744f0703658c661c1d65
Tommaso-Desiato <tommaso_desiato@libero.it>
2024-06-12 12:26:30 +0200

    Added font + restyle

--- src/index.js
+++ src/index.js
@@ -50,13 +50,13 @@ const fetchBooks = (event, offset = 0, isNewSearch = false) => {
 
       const returnTopBtn = document.createElement("button");
       returnTopBtn.innerHTML = `<button id="return-top"
-      class= "bg-indigo-600 text-white rounded-full w-10 h-10 shadow-lg hover:bg-indigo-700 transition transition-opacity opacity-0 pointer-events-none fixed bottom-10 right-4 flex items-center justify-center">
+      class= "bg-slate-950 text-white rounded-full w-10 h-10 shadow-lg hover:bg-slate-900 transition transition-opacity opacity-0 pointer-events-none fixed bottom-10 right-4 flex items-center justify-center">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
         </svg>
       </button>`;
       loadMoreBtn.innerHTML = `<button id="load-more"
-      class= "bg-indigo-600 text-white py-2 px-4 rounded">
+      class= "bg-slate-950 hover:bg-slate-900 shadow-lg text-white py-2 px-4 rounded">
       Load More
       </button>`;
       const loadMoreBtnContainer = document.getElementById(
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Book Finder",
+      title: "BookScout",
       template: "./src/index.html",
     }),
   ],
